9c518387710dc84d853d65e8917f333f333346a1
DaoMinhNhat <daominhnhat0403@gmail.com>
2024-05-24 14:11:00 +0700

    update

--- src/components/ui/modelTask.tsx
+++ src/components/ui/modelTask.tsx
@@ -11,7 +11,7 @@ type Props = {
   setIsModalOpen: any,
 
 }
-const ModelTask = ({isModalOpen,setIsModalOpen } : Props) => {
+const ModelTask = ({ isModalOpen, setIsModalOpen }: Props) => {
 
   // const handleOk = () => {
   //   setIsModalOpen(false);
@@ -23,16 +23,23 @@ const ModelTask = ({isModalOpen,setIsModalOpen } : Props) => {
 
   return (
     <>
-      <Modal  open={isModalOpen} footer={null}  onCancel={handleCancel}>
+      <Modal open={isModalOpen} footer={null} onCancel={handleCancel}>
         <div>
-          <div className='flex flex-col'>
-            <div  className='flex flex-col'>
-              <div className='flex flex-row justify-between'><p>Nhiệm vụ</p><span>icon</span></div>
-              <p>Hôm nay</p>
+          <div className='flex flex-col gap-6'>
+            <div className='flex flex-col gap-2'>
+              <div className='flex flex-row '><p className='font-semibold text-2xl text-blue-500'>Nhiệm vụ</p></div>
+              <p className='text-black '>Hôm nay</p>
+              <hr />
             </div>
 
-            <div className='flex flex-col'>
-            <Checkbox onChange={onChange}>Checkbox</Checkbox>
+            <div className='flex flex-col gap-5'>
+              <Checkbox defaultChecked={true} onChange={onChange}>Học từ vựng 800 - 850</Checkbox>
+              <Checkbox defaultChecked={true} onChange={onChange}>Học hán tự bài 7</Checkbox>
+              <Checkbox defaultChecked={true} onChange={onChange}>Học ngữ pháp 10 - 20</Checkbox>
+              <Checkbox onChange={onChange}>Luyện nghe</Checkbox>
+              <Checkbox onChange={onChange}>Làm bài tập về nhà</Checkbox>
+              <Checkbox onChange={onChange}>Chuẩn bị Mendan</Checkbox>
+
 
             </div>
           </div>
